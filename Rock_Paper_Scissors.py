4396b29ba2002a6b3c78b1d7777f5f574acc63ce
Ngan Nguyen <164316676+NannT0T@users.noreply.github.com>
2025-03-02 14:00:20 -0800

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,6 +1,6 @@
 # Project name: Rock-Paper-Scissors game
-# Discription: In this project we will generate a random number set (0, 1, 2) to represent (rock, paper, scissors) for the game. 
-             Create a while loop that simulates a game where the user competes against the computer. The user wins if their input matches one of the predefined winning options; otherwise, the computer wins.
+# Import: In this project we will generate a random number set (0, 1, 2) to represent (rock, paper, scissors) for the game. 
+# Discription: Create a while loop that simulates a game where the user competes against the computer. The user wins if their input matches one of the predefined winning options; otherwise, the computer wins.
 
                         
 import random
